f0bda941f3fa3980a21bee1bc31c4b3b8f79cc83
shown.Ji <yuluo08290126@gmail.com>
2025-06-29 18:37:55 +0800

    ci: update golangci-lint binary install dir
    Signed-off-by: shown.Ji <yuluo08290126@gmail.com>

--- tools/make/tools.mk
+++ tools/make/tools.mk
@@ -42,4 +42,4 @@ tools: ## Install ci tools
 	go install github.com/google/yamlfmt/cmd/yamlfmt@latest
 
 	@echo "Installing golangci-lint"
-	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/HEAD/install.sh | sh -s -- -b $(go env GOPATH)/bin v2.2.0
+	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/HEAD/install.sh | sh -s v2.2.0
